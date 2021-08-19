25fa25ce53a706ba4484b9de9b075633aed8c5a5
Sunil V L <sunilvl@ventanamicro.com>
2021-08-19 19:19:26 +0530

    Basic enablement of ACPI in RISC-V
    It just adds ACPI to the kernel build. Still more changes are
    required to boot.
    
    Signed-off-by: Sunil V L <sunilvl@ventanamicro.com>

--- arch/riscv/include/asm/io.h
+++ arch/riscv/include/asm/io.h
@@ -131,6 +131,13 @@ __io_writes_outs(writes, u64, q, __io_bw(), __io_aw())
 
 __io_writes_outs(outs, u64, q, __io_pbr(), __io_paw())
 #define outsq(addr, buffer, count) __outsq((void __iomem *)addr, buffer, count)
+
+/*
+ * I/O memory mapping functions.
+ */
+extern void __iomem *__ioremap(phys_addr_t phys_addr, size_t size, pgprot_t prot);
+extern void iounmap(volatile void __iomem *addr);
+extern void __iomem *ioremap_cache(phys_addr_t phys_addr, size_t size);
 #endif
 
 #include <asm-generic/io.h>
